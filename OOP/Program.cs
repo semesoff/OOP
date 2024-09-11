9d5751b11ccf487c43333e44a1e35ce8221853c9
semesoff <entertoofficial@gmail.com>
2024-09-11 12:42:34 +0300

    first

---
+++ OOP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP {
+
+    class Person {
+        private string name;
+        private int age;
+
+        public Person(string name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void DisplayInfo() {
+            Console.WriteLine($"Name: {name}\nAge: {age}");
+        }
+    }
+
+    class Program {
+        static void Main() {
+            Person person1 = new Person("Elon", 53);
+            person1.DisplayInfo();
+            Person person2 = new Person("Pavel", 39);
+            person2.DisplayInfo();
+        }
+    }
+
+}
